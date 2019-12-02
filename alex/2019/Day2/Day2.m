e3dc3976dd627bc516e6f2935d0792aff14a864b
are512 <35606670+are512@users.noreply.github.com>
2019-12-02 08:21:59 +0000

    Day 2 working

---
+++ alex/2019/Day2/Day2.m
@@ -0,0 +1,42 @@
+output = 0;
+while output ~= 19690720
+    for noun = 0:99
+        for verb = 0:99
+            day2 = fopen('input.txt','r');
+            day2Data = textscan(day2, '%d', 'Delimiter', ',');
+            day2Data = day2Data{1};
+            day2Data = transpose(day2Data);
+            fclose(day2);
+            day2Data(2) = noun;
+            day2Data(3) = verb;
+            for i = 1:4:149
+                pos1 = day2Data(i+1)+1;
+                pos2 = day2Data(i+2)+1;
+                outpos = day2Data(i+3)+1;
+                if day2Data(i) == 1
+                    day2Data(outpos) = day2Data(pos1) + day2Data(pos2);
+                elseif day2Data(i) == 2
+                    day2Data(outpos) = day2Data(pos1) * day2Data(pos2);
+                elseif day2Data(i) == 99
+                    output = day2Data(1);
+                    break
+                end
+                if output == 19690720
+                    break
+                end
+            end
+            if output == 19690720
+               break
+            end
+        end
+        if output == 19690720
+            break
+        end
+    end
+    if output == 19690720
+        break
+    end
+end
+
+
+            
