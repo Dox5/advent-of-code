4f554852564afcb18af00b4cb3f2c6c7dab22303
Thurstan G <1319936+grayter@users.noreply.github.com>
2019-12-14 15:39:18 +0000

    Tidy code

--- treg/day14.py
+++ treg/day14.py
@@ -4,43 +4,36 @@ import fileinput
 import math
 import collections
 
-created = collections.defaultdict(int)
-totalOre = 0
 
-def normalProduction(reactions, target, amount, f):
-    # Do we already have the amount required?
-    if created[target] >= amount:
-        return
-
-    # How much extra stuff do we need
-    recipe = reactions[target]
-    required = amount - created[target]
-    multiplier = math.ceil(required / recipe[0])      
-        
-    for part in recipe[1]:
-        newAmount = part[0]*multiplier
-        f(reactions, part[1], newAmount)
-        # Remember that we've consumed whatever we've needed before next req
-        created[part[1]] -= (part[0]*multiplier)
-            
-    # And we produce our stuff
-    created[target] += (recipe[0]*multiplier)
-        
-def getOreRequirements(reactions, target, amount):
-    global totalOre
+def getOreCount(reactions, target, amount):
+    created = collections.defaultdict(int)    
+    return getOreRequirement(reactions, target, amount, created)
+    
+def getOreRequirement(reactions, target, amount, created):
+    generatedOre = 0
     if target == "ORE":
         created["ORE"] += amount
-        totalOre += amount
+        return amount
     else:
-        normalProduction(reactions, target, amount, getOreRequirements)
+        # Do we already have the amount required?
+        if created[target] >= amount:
+            return 0
 
-def exhaustOre(reactions, target, amount):
-    if target == "ORE":
-        if amount > created["ORE"]:
-            raise ArithmeticError("Too little ORE")
-    else:
-        normalProduction(reactions, target, amount, exhaustOre)
+        # How much extra stuff do we need
+        recipe = reactions[target]
+        required = amount - created[target]
+        multiplier = math.ceil(required / recipe[0])      
         
+        for part in recipe[1]:
+            newAmount = part[0]*multiplier
+            generatedOre += getOreRequirement(reactions, part[1], newAmount, created)
+            # Remember that we've consumed whatever we've needed before next req
+            created[part[1]] -= (part[0]*multiplier)
+            
+        # And we produce our stuff
+        created[target] += (recipe[0]*multiplier)
+        return generatedOre
+    
 def main():
     reactions = {}
     rstr = list(fileinput.input())
@@ -55,37 +48,19 @@ def main():
         # Resultant
         q, w = r[1].split(' ')
         reactions[w] = (int(q), ingredients)
-        
-    getOreRequirements(reactions, "FUEL", 1)
-    print(totalOre)
 
-    global created
-    reqFuel = 1
+    # Part 1
+    print(getOreCount(reactions, "FUEL", 1))
 
-    # Start from the bottom and double requests until we fail
-    while True:
-        created = collections.defaultdict(int)
-        created["ORE"] = 1000000000000            
-        try:
-            exhaustOre(reactions, "FUEL", reqFuel)
-            # Success if here
-            reqFuel *= 2
-        except:
-            # Failed, so min bound is
-            reqFuel /= 2
-            break
-
-    # We know a lower and upper bound in powers of two now split them
-    lower = reqFuel
-    upper = reqFuel * 2
+    # Part 2
+    # Take some extreme bounds and binary search, checking if too much ore is used
+    lower = 1
+    upper = 1000000000000
 
     while True:
-        created = collections.defaultdict(int)
-        created["ORE"] = 1000000000000                    
         middle = int((lower+upper) // 2)
         
-        try:
-            exhaustOre(reactions, "FUEL", middle)
+        if getOreCount(reactions, "FUEL", middle) <= 1000000000000:            
             # Success. Let middle be our new lower, but check if done first
             if lower == middle:
                 # Sovled
@@ -93,8 +68,7 @@ def main():
                 break
 
             lower = middle
-            
-        except:
+        else:
             # O dear. Let middle be ouer new upper
             upper = middle
     
