33d7c768744832c8c26472e936711b569c1346cb
Dox5 <doxfive@gmail.com>
2021-12-02 21:58:47 +0000

    go fmt

--- james/2021/aoc_fetch/fetch.go
+++ james/2021/aoc_fetch/fetch.go
@@ -1,106 +1,104 @@
 package aoc_fetch
 
 import (
-    "os"
-    "fmt"
-    "crypto/tls"
-    "net/http"
-    "io"
-    "io/ioutil"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func addCookie(req *http.Request) error {
-    if token, ok := os.LookupEnv("AOC_TOKEN") ; ok {
-        req.AddCookie(&http.Cookie{
-            Name: "session",
-            Value: token,
-        })
-        return nil
-    } else {
-        return fmt.Errorf("Set AOC_TOKEN to fetch input")
-    }
+	if token, ok := os.LookupEnv("AOC_TOKEN"); ok {
+		req.AddCookie(&http.Cookie{
+			Name:  "session",
+			Value: token,
+		})
+		return nil
+	} else {
+		return fmt.Errorf("Set AOC_TOKEN to fetch input")
+	}
 }
 
 func cacheFilepath(year, day string) (string, error) {
-    user_cache_dir, err := os.UserCacheDir()
+	user_cache_dir, err := os.UserCacheDir()
 
-    if err != nil {
-        return "", fmt.Errorf("Failed to determine cache path: %v", err)
-    }
+	if err != nil {
+		return "", fmt.Errorf("Failed to determine cache path: %v", err)
+	}
 
-    file := fmt.Sprintf("%s/advent-of-code-input-%s-%s", user_cache_dir, year, day)
-    return file, err
+	file := fmt.Sprintf("%s/advent-of-code-input-%s-%s", user_cache_dir, year, day)
+	return file, err
 }
 
 func checkCache(year, day string) ([]byte, error) {
-    cache_path, err := cacheFilepath(year, day)
+	cache_path, err := cacheFilepath(year, day)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-
-    contents, err := ioutil.ReadFile(cache_path)
-    return contents, err
+	contents, err := ioutil.ReadFile(cache_path)
+	return contents, err
 }
 
 func cacheInput(year, day string, contents []byte) error {
-    cache_path, err := cacheFilepath(year, day)
+	cache_path, err := cacheFilepath(year, day)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    return ioutil.WriteFile(cache_path, contents, 0444)
+	return ioutil.WriteFile(cache_path, contents, 0444)
 }
 
 func fetchInput(year, day string) ([]byte, error) {
-    client := &http.Client{
-        Transport: &http.Transport {
-            TLSClientConfig: &tls.Config { },
-        },
-    }
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{},
+		},
+	}
 
-    url := fmt.Sprintf("https://adventofcode.com/%s/day/%s/input", year, day)
-    req, err := http.NewRequest("GET", url, nil)
+	url := fmt.Sprintf("https://adventofcode.com/%s/day/%s/input", year, day)
+	req, err := http.NewRequest("GET", url, nil)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    if err = addCookie(req); err != nil {
-        return nil, err
-    }
+	if err = addCookie(req); err != nil {
+		return nil, err
+	}
 
-    resp, err := client.Do(req)
-    if err != nil {
-        return nil, err
-    }
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
 
-    contents, err := io.ReadAll(resp.Body)
-    return contents, err
+	contents, err := io.ReadAll(resp.Body)
+	return contents, err
 }
 
-
 func PuzzleInputFor(year, day string) ([]byte, error) {
-    contents, err := checkCache(year, day);
+	contents, err := checkCache(year, day)
 
-    if err == nil {
-        fmt.Printf("Found cached input for %v %v\n", year, day)
-        return contents, nil
-    } else {
-        fmt.Printf("No cached version found, will try to fetch: %v\n", err)
-    }
+	if err == nil {
+		fmt.Printf("Found cached input for %v %v\n", year, day)
+		return contents, nil
+	} else {
+		fmt.Printf("No cached version found, will try to fetch: %v\n", err)
+	}
 
-    contents, err = fetchInput(year, day)
+	contents, err = fetchInput(year, day)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    if err = cacheInput(year, day, contents); err != nil {
-        return nil, err
-    }
+	if err = cacheInput(year, day, contents); err != nil {
+		return nil, err
+	}
 
-    return contents, nil
+	return contents, nil
 }
--- james/2021/cmd/day01/main.go
+++ james/2021/cmd/day01/main.go
@@ -1,100 +1,100 @@
 package main
 
 import (
-    "github.com/Dox5/advent-of-code/james/2021/aoc_fetch"
-    "fmt"
-    "bytes"
-    "bufio"
-    "strconv"
+	"bufio"
+	"bytes"
+	"fmt"
+	"github.com/Dox5/advent-of-code/james/2021/aoc_fetch"
+	"strconv"
 )
 
 func part1(depths []int) {
-    increases := 0
+	increases := 0
 
-    last := depths[0]
+	last := depths[0]
 
-    for _, depth := range(depths[1:]) {
-        if depth > last {
-            increases++
-        }
-        last = depth
-    }
+	for _, depth := range depths[1:] {
+		if depth > last {
+			increases++
+		}
+		last = depth
+	}
 
-    fmt.Printf("There were %v increases\n", increases)
+	fmt.Printf("There were %v increases\n", increases)
 }
 
 func sum(vs []int) int {
-    accum := 0
-    for _, v := range vs {
-        accum += v
-    }
+	accum := 0
+	for _, v := range vs {
+		accum += v
+	}
 
-    return accum
+	return accum
 }
 
 func part2(depths []int) {
-    windowLen := 3
-    windowCount := len(depths) - windowLen + 1
+	windowLen := 3
+	windowCount := len(depths) - windowLen + 1
 
-    increases := 0
+	increases := 0
 
-    last := sum(depths[0:3])
-    for i := 1 ; i < windowCount ; i++ {
-        this := sum(depths[i:i+3])
+	last := sum(depths[0:3])
+	for i := 1; i < windowCount; i++ {
+		this := sum(depths[i : i+3])
 
-        if this > last {
-            increases++
-        }
+		if this > last {
+			increases++
+		}
 
-        last = this
-    }
+		last = this
+	}
 
-    fmt.Printf("There were %v increases\n", increases)
+	fmt.Printf("There were %v increases\n", increases)
 }
 
 func parseDepths(input []byte) ([]int, error) {
-    scanner := bufio.NewScanner(bytes.NewReader(input))
-    depths := make([]int, 0, 500)
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+	depths := make([]int, 0, 500)
 
-    for scanner.Scan() {
-        v, err := strconv.ParseInt(scanner.Text(), 10, 32)
+	for scanner.Scan() {
+		v, err := strconv.ParseInt(scanner.Text(), 10, 32)
 
-        if err != nil {
-            return nil, fmt.Errorf("Failed to convert %v to an integer: %v", scanner.Text(), err)
-        }
+		if err != nil {
+			return nil, fmt.Errorf("Failed to convert %v to an integer: %v", scanner.Text(), err)
+		}
 
-        depths = append(depths, int(v))
-    }
+		depths = append(depths, int(v))
+	}
 
-    return depths, nil
+	return depths, nil
 }
 
 func main() {
-    raw_input, err := aoc_fetch.PuzzleInputFor("2021", "1")
-    if err != nil {
-        fmt.Println("Failed to load input,", err)
-    }
+	raw_input, err := aoc_fetch.PuzzleInputFor("2021", "1")
+	if err != nil {
+		fmt.Println("Failed to load input,", err)
+	}
 
-    depths, err := parseDepths(raw_input)
-    if err != nil {
-        fmt.Println("Failed to get list of depths:", err)
-        return
-    }
+	depths, err := parseDepths(raw_input)
+	if err != nil {
+		fmt.Println("Failed to get list of depths:", err)
+		return
+	}
 
-    exmpleData := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+	exmpleData := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 
-    fmt.Println("#### PART 1 ####")
-    fmt.Println("Example Data")
-    part1(exmpleData)
+	fmt.Println("#### PART 1 ####")
+	fmt.Println("Example Data")
+	part1(exmpleData)
 
-    fmt.Println("Real")
-    part1(depths)
+	fmt.Println("Real")
+	part1(depths)
 
-    fmt.Println()
-    fmt.Println("#### PART 2 ####")
-    fmt.Println("Example Data")
-    part2(exmpleData)
+	fmt.Println()
+	fmt.Println("#### PART 2 ####")
+	fmt.Println("Example Data")
+	part2(exmpleData)
 
-    fmt.Println("Real")
-    part2(depths)
+	fmt.Println("Real")
+	part2(depths)
 }
