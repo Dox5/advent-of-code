344ced98d138da5406b4819b018e575611ac3bff
Dox5 <doxfive@gmail.com>
2021-12-05 16:15:23 +0000

    Redid it and it do now

--- james/2021/cmd/day05/main.go
+++ james/2021/cmd/day05/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/Dox5/advent-of-code/james/2021/aoc_fetch"
 	"github.com/alecthomas/participle/v2"
 	"fmt"
-	"sort"
 )
 
 var exampleData = `0,9 -> 5,9
@@ -34,180 +34,133 @@ type HydroThermalVents struct {
 
 var parser = participle.MustBuild(&HydroThermalVents{})
 
-func splitByPlane(vents *HydroThermalVents) (horrizontal, vertical, others []Line) {
-	horrizontal = make([]Line, 0, len(vents.VentLocations))
-	vertical = make([]Line, 0, len(vents.VentLocations))
-	others = make([]Line, 0, len(vents.VentLocations))
+// Ensure that a < b
+func increasing(a, b int) (int, int) {
+	if a > b {
+		return b, a
+	} else {
+		return a, b
+	}
+}
 
-	for _, line := range vents.VentLocations {
-		if line.Start.X == line.End.X && line.Start.Y == line.End.Y {
-			panic("Cruel - a single point, is it horrizontal or vertical?!")
+// What increment to use 1 or -1 to get from a to b one step at a time. If a == b
+// return 0, no increment needed
+func stepBetween(a, b int) (int, int) {
+	if a < b {
+		return 1, b - a + 1;
+	} else if a > b {
+		return -1, a - b + 1;
+	} else {
+		return 0, 0
+	}
+}
+
+func pickStepCount(a, b int) int {
+	if a == b {
+		return a
+	} else if b == 0 {
+		return a
+	} else if a == 0 {
+		return b
+	}
+
+	panic("Can't cope!")
+}
+
+// For every line, write out every coordinate it touches to the channel.
+func alignedLineToPoints(vents []Line, points chan<-Point, allowDiag bool) {
+	defer close(points)
+	for _, ventLine := range vents {
+
+		isHorrizontal := ventLine.Start.Y == ventLine.End.Y
+		isVirtical := ventLine.Start.X == ventLine.End.X
+		if !allowDiag && !(isHorrizontal || isVirtical) {
+			continue
 		}
 
-		switch {
-		case line.Start.Y == line.End.Y:
-			horrizontal = append(horrizontal, line)
-		case line.Start.X == line.End.X:
-			vertical = append(vertical, line)
-		default:
-			others = append(others, line)
+		// Walk between the two points and output them all
+		xInc, xSteps := stepBetween(ventLine.Start.X, ventLine.End.X)
+		yInc, ySteps := stepBetween(ventLine.Start.Y, ventLine.End.Y)
+		x := ventLine.Start.X
+		y := ventLine.Start.Y
+
+		steps := pickStepCount(xSteps, ySteps)
+		for i := 0; i < steps; i++ {
+			point := Point{X: x, Y: y}
+			points <- point
+
+			x += xInc
+			y += yInc
+		}
+	}
+}
+
+func countByPoint(points <-chan Point) (counts map[Point]int) {
+	counts = make(map[Point]int)
+	for point := range points {
+		if c, ok := counts[point] ; ok {
+			counts[point] = c + 1
+		} else {
+			counts[point] = 1
 		}
 	}
 
 	return
 }
 
+func calculateOverlaps(vents []Line, allowDiag bool) {
+	points := make(chan Point, 32)
 
-// Return the comparison, differentiation for the overlapping check
-// eg this would return X, Y for a horrizontal check and Y, X for a vertical one
-type CompareAndDiff func(Point) (int, int)
+	go alignedLineToPoints(vents, points, allowDiag)
+	counts := countByPoint(points)
+	overlappedPoints := 0
 
-type overlapEntry struct {
-	secondary int
-	overlaps int
-}
-
-type OverlapTracker struct {
-	overlaps []overlapEntry
-}
-
-func NewOverlapTracker() (*OverlapTracker) {
-	return &OverlapTracker {
-		overlaps: make([]overlapEntry, 0, 100),
-	}
-}
-
-// Apply the modifier to the tracker
-func (ot *OverlapTracker) Apply(secondary, modifier int) {
-	at := sort.Search(len(ot.overlaps), func(i int) bool {
-		return ot.overlaps[i].secondary >= secondary
-	})
-
-	if at < len(ot.overlaps) && ot.overlaps[at].secondary == secondary {
-		ot.overlaps[at].overlaps += modifier
-	} else {
-		// Create a new entry
-		ot.overlaps = append(ot.overlaps, overlapEntry{})
-		copy(ot.overlaps[at+1:], ot.overlaps[at:])
-		ot.overlaps[at] = overlapEntry { secondary: secondary, overlaps: modifier }
-	}
-}
-
-// Count where overlaps > 1
-func (ot *OverlapTracker) CountOverlaps() int {
-	count := 0
-	for _, overlap := range ot.overlaps {
-		if overlap.overlaps > 1 {
-			count++
+	for _, v := range counts {
+		if v > 1 {
+			overlappedPoints++
 		}
 	}
 
-	return count
-}
-
-func countOverlaps(vents []Line, fn CompareAndDiff) int {
-	// Bad name - this records when each vent starts and stops
-	type X struct {
-		coord int // pos in the axis we're looking at
-		diff  int // pos in the other axis
-		modifier int // +1 or -1 to adjust number of overlapping vents
-	}
-
-	algo := make([]X, 0, len(vents)*2)
-
-
-	// Populate the structure first
-	for _, vent := range vents {
-		start, otherAxis := fn(vent.Start)
-		end, _ := fn(vent.End) // other axis is always the same
-		algo = append(algo,
-			X{coord: start, diff: otherAxis, modifier: 1}, // Start happens where we 'expect'
-			X{coord: end, diff: otherAxis, modifier: -1})
-	}
-
-	sort.Slice(algo, func(i, j int) bool {
-		// Sort by primary access first - then secondary for dedupe purposes!
-		if algo[i].coord < algo[j].coord {
-			return true
-		} else if algo[i].coord == algo[j].coord {
-			return algo[i].diff <= algo[j].diff
-		} else {
-			return false
-		}
-
-	})
-
-	// Now collapse out duplicate coordinates
-	k := 0
-	for _, maybeDupe := range algo[1:] {
-		if maybeDupe.diff == algo[k].diff && maybeDupe.coord == algo[k].coord {
-			// Duplicate
-			algo[k].modifier += maybeDupe.modifier
-		} else {
-			// New unique vent, move on the 'write' pointer and put this vent
-			// here
-			k++
-			algo[k] = maybeDupe
-		}
-	}
-	algo = algo[:k]
-
-	fmt.Printf("Algo data: %+v\n", algo)
-
-	tracker := NewOverlapTracker()
-
-	totalOverlaps := 0
-
-	// Sweep the axis 0 -> <end>
-	lastPrimary := 0
-	for _, change := range algo {
-		fmt.Printf("change: %+v\n", change)
-		overlappedInLastRange := tracker.CountOverlaps()
-		lastJumpLen := change.coord - lastPrimary + 1
-		lastPrimary = change.coord
-
-		totalOverlaps += overlappedInLastRange * lastJumpLen
-
-		// Always apply updats last
-		tracker.Apply(change.diff, change.modifier)
-
-	}
-
-	return totalOverlaps
+	fmt.Println("Overlapping points:", overlappedPoints)
 }
 
 func part1(vents *HydroThermalVents) {
-	_, vertical, _ := splitByPlane(vents)
-	results := make(chan int)
+	calculateOverlaps(vents.VentLocations, false)
+}
 
-	//go func() {
-	//	hOverlap := countOverlaps(horrizontal, func(p Point) (int, int) { return p.X, p.Y})
-	//	results <- hOverlap
-	//}()
-
-	go func() {
-		vOverlap := countOverlaps(vertical, func(p Point) (int, int) { return p.Y, p.X})
-		results <- vOverlap
-	}()
-
-	totalOverlaps := 0
-	for i := 0; i < 1; i++ {
-		totalOverlaps += <- results
-	}
-
-	fmt.Println("Found", totalOverlaps, "overlaps")
+func part2(vents *HydroThermalVents) {
+	calculateOverlaps(vents.VentLocations, true)
 }
 
 func main() {
+	raw_input, err := aoc_fetch.PuzzleInputFor("2021", "5")
+	if err != nil {
+		fmt.Println("Failed to load input,", err)
+	}
+
+	exampleVents := &HydroThermalVents{};
 	vents := &HydroThermalVents{};
-	err := parser.ParseString("exampleData", exampleData, vents)
+	err = parser.ParseString("exampleData", exampleData, exampleVents)
 	if err != nil {
 		fmt.Println("Error parsing imput:", err)
 		return
 	}
 
+	err = parser.ParseBytes("realData", raw_input, vents)
+	if err != nil {
+		fmt.Println("Error parsing imput:", err)
+		return
+	}
+
+	fmt.Println("##### Part1 #####")
+	fmt.Println("   ExampleData")
+	part1(exampleVents)
+	fmt.Println("     RealData")
 	part1(vents)
 
-	fmt.Printf("%+v\n", vents)
-
+	fmt.Println("##### Part2 #####")
+	fmt.Println("   ExampleData")
+	part2(exampleVents)
+	fmt.Println("     RealData")
+	part2(vents)
 }
